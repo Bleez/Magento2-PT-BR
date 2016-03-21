49f95fc2dcd3166f03ecca99387e2ae249d31428
Bruno Viana <brunoviana@gmail.com>
2016-03-21 14:09:47 +0000

    Traducao das internas do cadastro de produto

--- registration.php
+++ registration.php
@@ -1,7 +1,6 @@
 <?php
-
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'bleez_pt_br',
-    __DIR__
-);
+        \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+        'bleez_pt_br',
+        __DIR__
+    );
