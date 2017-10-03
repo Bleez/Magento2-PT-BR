2952b7b9734764bce4c75109f5493eb438e45b3e
Carlos Gartner <cagartner@MacBook-Air-de-Carlos.local>
2017-10-03 14:32:18 -0300

    fixed some name mistakes

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-        'bleez_pt_br',
+        'trezoteam_pt_br',
         __DIR__
     );
